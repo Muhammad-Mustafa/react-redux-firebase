77c757aa4730c395463b8dd772df3a6e9275ea7d
Muhammad-Mustafa <mustafaraziq30@gmail.com>
2020-08-18 22:44:47 +0500

    half UI Implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+//IMPORTING COMPONENTS
+import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
+import ProjectDetails from "./components/projects/ProjectDetails";
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/project/:id" component={ProjectDetails} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
